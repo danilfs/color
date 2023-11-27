8e50ce3f9af900d7ae3f3e9df4853cd3c6e7174f
dsfrantsev <fds@avito.ru>
2023-11-27 22:33:04 +0300

    color id

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const cols = document.querySelectorAll('.col')
 
-function generateRandomColor() {
-    hexCodes = '123456789ABCDEF'
-    let color = ''
-    for(let i = 0; i < 6; i++) {
-        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+function generateRandomColor () {
+    hexCode = '123456789ABCDEF'
+    color = ''
+    for(let i = 0; i < 6 ; i++) {
+        color += hexCode[Math.floor(Math.random() * hexCode.length)]
     }
     return '#' + color
 }
 
 function setRandomColor () {
     cols.forEach((col) => {
-        col.style.background = generateRandomColor() 
+
+        const text = col.querySelector('h2')
+        const color = generateRandomColor()
+
+        text.textContent = color
+        col.style.backgroundColor = generateRandomColor()
     })
 }
 
-setRandomColor()
-
-
+setRandomColor() 
